e9de76af0cc94b718d5a85a55ec49337ba322ae8
enikar <enikar@gresille.org>
2025-01-20 14:48:02 +0100

    Rewriting. We use IntSet instead of a binary search on a Vector.

--- hday1.hs
+++ hday1.hs
@@ -1,46 +1,91 @@
+-- AoC 2020, day 1. Rewritten on January 20, 2025
+
+-- The aim of the puzzle is to find 2 then 3 numbers whose sum is 2020
+-- from the list of numbers contained in the file "day1.txt" and compute
+-- their products.
 
 module Main  where
 
-import Data.Vector.Unboxed (fromList, (!), Vector)
-import qualified Data.Vector.Unboxed as V
+import System.IO (readFile')
+import qualified Data.Vector.Unboxed as V -- package vector
+import Data.Vector.Unboxed
+  (Vector
+  ,(!)
+  )
+import qualified Data.IntSet as S  -- package containers
 import Data.Functor ((<&>))
 import Data.Foldable (find)
 import Data.Maybe (fromMaybe)
 import Text.Read (readMaybe)
 import Control.Monad.ST (runST)
-import Data.Vector.Algorithms.Heap (sort)
-import Control.Monad.Extra (loop)
+import Data.Vector.Algorithms.Heap (sort) -- package vector-algorithms
+import Control.Monad.Extra (loop) -- package extra
 
+
+-- The input file "day1.txt" contains one number per line.
+-- So we read all lines, and convert each string to an Int.
 readDatas :: IO (Vector Int)
 readDatas = do
-  content <- readFile "day1.txt"
-  let errRead = error "ReadDatas: not an Int in input.txt"
-      readInt = fromMaybe errRead . readMaybe
-      numbers = fromList  (map readInt (words content))
-      sorted = runST $ do
-        v <- V.unsafeThaw numbers
+  content <- readFile' "day1.txt"
+  let errorRead = error "ReadDatas: not an Int in day1.txt."
+      readInt = fromMaybe errorRead . readMaybe
+      numbers = map readInt (words content)
+      -- Sort the vector makes the part1 and part2 faster.
+      vec = runST $ do
+        v <- V.unsafeThaw (V.fromList numbers)
         sort v
         V.unsafeFreeze v
-  pure sorted
+  pure vec
 
 main :: IO ()
 main = do
   datas <- readDatas
   let combs = combinationsOpt 2 datas
-  showSolution "Part1" (part1 combs)
-  showSolution "Part2" (part2 datas combs)
+  printSolution "Part1" (part1 combs)
+  printSolution "Part2" (part2' datas combs)
 
-showSolution :: String -> [Int] -> IO ()
-showSolution part sol =
+printSolution :: String -> [Int] -> IO ()
+printSolution part sol =
   putStrLn $ part <> ": numbers: " <> show sol <> ".  Product: " <> show p
       where
         p = product sol
 
+-- Part1 returns a list of 2 Ints whose sum is 2020.
 part1 :: [[Int]] -> [Int]
-part1 = fromMaybe errPart1 . find  ((==2020) . sum)
+part1 = fromMaybe showError . find  ((==2020) . sum)
   where
-    errPart1 = error "Part1: no solution found"
+    showError = error "Internal error: part1"
 
+-- part2 returns a list of 3 Ints whose sum is 2020.
+-- Using foldr to short-circuit. Foldr stops looping when we
+-- we don't use acc anymore.
+part2 :: Vector Int -> [[Int]] -> [Int]
+part2 datas = foldr f []
+  where
+    numbers = S.fromAscList (V.toList datas)
+    f comb acc
+      |S.member n numbers = n:comb
+      |otherwise          = acc
+        where n = 2020 - sum comb
+
+-- With loop from Control.Monad.Extra the short-circuit is more
+-- explicit. We leave the loop when when we return a Right, and
+-- continue looping as long as we return Left.
+part2' :: Vector Int -> [[Int]] -> [Int]
+part2' datas = loop search
+  where
+    numbers = S.fromAscList (V.toList datas)
+
+    search [] = error "Error: part2: no solution found"
+    search (comb:combs)
+      | S.member n numbers = Right (n:comb)
+      | otherwise          = Left combs
+        where
+          n = 2020 - sum comb
+
+-- combinationsOpt creates a list of all combinations of k Ints from the
+-- input vector vec. We proceed by eploring all possibilities (it is
+-- a backtracking algorithm).
 combinationsOpt :: Int -> Vector Int -> [[Int]]
 combinationsOpt k vec
   | k < 0      = error "combinationsOpt: bad arguments"
@@ -52,46 +97,3 @@ combinationsOpt k vec
         | otherwise  = [start..n] >>= \i -> go (i+1) (depth+1) (consComb i)
                where
                  consComb i = vec ! i : comb
-
--- part2 ::  Vector Int -> [[Int]] -> [Int]
--- part2 vec = foldr f []
---   where
---     f comb acc = case binarySearch vec (2020 - sum comb) of
---                    Nothing -> acc
---                    Just v -> v:comb
-part2 ::  Vector Int -> [[Int]] -> [Int]
-part2 vec = loop search
-  where
-    search []           = error "Part2: no solution found"
-    search (comb:combs) = case binarySearch vec (2020 - sum comb) of
-                                Nothing -> Left combs
-                                Just e  -> Right (e:comb)
-
--- v est un Vector Int trié par ordre croissant.
--- e est la valeur à chercher.
--- renvoie (Just e) ou Nothing. Mais pas l'indice dans le Vector !
-binarySearch :: Vector Int -> Int -> Maybe Int
-binarySearch v e
-  | v ! sup0 < e = Nothing
-  | otherwise    = go 0 sup0
-  where
-    sup0 = V.length v - 1
-
-    go inf sup
-      | inf >= sup  = if v ! inf == e then Just e else Nothing
-      | v ! mid < e = go (mid+1) sup
-      | otherwise   = go inf mid
-        where mid = (inf+sup) `div` 2
-
--- binarySearch :: Vector Int -> Int -> Maybe Int
--- binarySearch v e
---   | v ! sup0 < e = Nothing
---   | otherwise    = loop f (0, sup0)
---   where
---     sup0 = V.length v - 1
-
---     f (inf, sup)
---       | inf >= sup  = Right (if v ! inf == e then Just e else Nothing)
---       | v ! mid < e = Left (mid+1, sup)
---       | otherwise   = Left (inf, mid)
---         where mid = (inf+sup) `div` 2
