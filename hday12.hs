4b598e579f8c6264bee8620bf9861551ff9b35f8
enikar <enikar@gresille.org>
2025-02-15 21:08:06 +0100

    Some improvements

--- hday12.hs
+++ hday12.hs
@@ -2,8 +2,6 @@
 
 -- Very bad code for a painfull puzzle!
 
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE MultiWayIf #-}
 {- HLINT ignore "Eta reduce" -}
 
 import System.IO (readFile')
@@ -15,18 +13,17 @@ import Text.Read (readMaybe)
 printSolution :: Show a => String -> a -> IO ()
 printSolution part sol = putStrLn (part <> ": " <> show sol)
 
-data Instruction = N Int
-                   |S Int
-                   |W Int
-                   |E Int
-                   |L Int
-                   |R Int
+data Instruction = NS Int
+                   |EW Int
+                   |RL Int
                    |F Int deriving (Show)
 
 
 -- orientation: East=0, South=90, West=180, North=270
+-- In coordinates West, and South are negative, East and North
+-- are positive.
 data Ship = Ship {orientation :: Int
-                 ,position :: (Int, Int)
+                 ,position :: (Int, Int) -- (east, north)
                  ,waypoint :: (Int,Int) -- (east,north)
                  } deriving (Show)
 
@@ -36,12 +33,12 @@ getDatas filename = parseInstructions <$> readFile' filename
     parseInstructions str = map parseInstr (lines str)
     parseInstr (x:xs) =
       case x of
-        'N' -> N (readInt xs)
-        'S' -> S (readInt xs)
-        'W' -> W (readInt xs)
-        'E' -> E (readInt xs)
-        'L' -> L (readInt xs)
-        'R' -> R (readInt xs)
+        'N' -> NS (readInt xs)
+        'S' -> NS (negate (readInt xs))
+        'W' -> EW (negate (readInt xs))
+        'E' -> EW (readInt xs)
+        'L' -> RL (negate (readInt xs))
+        'R' -> RL (readInt xs)
         'F' -> F (readInt xs)
         _   -> errorParse
     parseInstr _ = errorParse
@@ -65,44 +62,41 @@ ship0 = Ship {orientation = 0
              ,position = (0,0)
              ,waypoint = (10, 1)
              }
+
 solve :: (Ship -> Instruction -> Ship) -> [Instruction] -> Int
 solve move instrs = abs east + abs north
   where
     (east, north) = position (foldl' move ship0 instrs)
 
+-- two nested cases… it's ugly
 move1 :: Ship -> Instruction -> Ship
-move1 ship@(Ship {..}) instr =
-  let (ew, ns) = position
-      o = orientation
+move1 ship instr =
+  let (ew, ns) = position ship
+      o = orientation ship
   in case instr of
-  (N m) -> ship {position = (ew,ns+m)}
-  (S m) -> ship {position = (ew, ns-m)}
-  (W m) -> ship {position = (ew-m, ns)}
-  (E m) -> ship {position = (ew+m, ns)}
-  (L m) -> ship {orientation = (o-m+360) `mod` 360}
-  (R m) -> ship {orientation = (o+m) `mod` 360}
-  (F m) -> if |o == 0 -> ship {position = (ew+m, ns)}
-              |o == 90 -> ship {position = (ew, ns-m)}
-              |o == 180 -> ship {position = (ew-m, ns)}
-              |o == 270 -> ship {position = (ew, ns+m)}
-              |otherwise -> error "Error: move1: bad orientation"
+  (NS m) -> ship {position = (ew,ns+m)}
+  (EW m) -> ship {position = (ew+m, ns)}
+  (RL m) -> ship {orientation = (o+m+360) `rem` 360}
+  (F m) -> case o of
+             0   -> ship {position = (ew+m, ns)}
+             90  -> ship {position = (ew, ns-m)}
+             180 -> ship {position = (ew-m, ns)}
+             270 -> ship {position = (ew, ns+m)}
+             _   -> error "Error: move1: bad orientation"
 
 move2 :: Ship -> Instruction -> Ship
-move2 ship@(Ship{..}) instr =
-  let (ew, ns) = position
-      (wayE, wayN) = waypoint
-      errorMove2 = error "Error: move2: bad rotation"
+move2 ship instr =
+  let (ew, ns) = position ship
+      (wayE, wayN) = waypoint ship
   in case instr of
-    (N m) -> ship {waypoint = (wayE, wayN+m)}
-    (S m) -> ship {waypoint = (wayE, wayN-m)}
-    (W m) -> ship {waypoint = (wayE - m, wayN)}
-    (E m) -> ship {waypoint = (wayE + m, wayN)}
+    (NS m) -> ship {waypoint = (wayE, wayN+m)}
+    (EW m) -> ship {waypoint = (wayE + m, wayN)}
     (F m) -> ship {position = (ew + m*wayE, ns + m*wayN)}
-    (R m) -> if |m == 90  -> ship {waypoint = (wayN, -wayE)}
-                |m == 180 -> ship {waypoint = (-wayE, -wayN)}
-                |m == 270 -> ship {waypoint = (-wayN, wayE)}
-                |otherwise -> errorMove2
-    (L m) -> if |m == 90  -> ship {waypoint = (-wayN, wayE)}
-                |m == 180 -> ship {waypoint = (-wayE, -wayN)}
-                |m == 270 -> ship {waypoint = (wayN, -wayE)}
-                |otherwise -> errorMove2
+    (RL m) -> case m of
+                90   -> ship {waypoint = (wayN, -wayE)}
+                180  -> ship {waypoint = (-wayE, -wayN)}
+                270  -> ship {waypoint = (-wayN, wayE)}
+                -90  -> ship {waypoint = (-wayN, wayE)}
+                -180 -> ship {waypoint = (-wayE, -wayN)}
+                -270 -> ship {waypoint = (wayN, -wayE)}
+                _    -> error "Error: move2: bad rotation"
