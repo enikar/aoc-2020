6d2878cbc91dcf575d91c4d7834d268ae9846405
enikar <enikar@gresille.org>
2025-02-19 02:41:30 +0100

    Add a solution for day 14

---
+++ hday14.hs
@@ -0,0 +1,169 @@
+-- AoC 2020, day 14
+
+-- this puzzle was not very appealing and astonishingly easy. Thanks to
+-- the Applicative instance of List. I would have expected the program to
+-- take longer and the results to be greater, but they fit into an Int64.
+
+{-# LANGUAGE ImportQualifiedPost #-}
+
+{- HLINT ignore "Eta reduce" -}
+
+import System.IO (readFile')
+import Data.List
+  (foldl'
+  ,unfoldr
+  )
+import Data.Bits
+  ((.&.)
+  ,(.|.)
+  )
+import Data.IntMap qualified as M
+import Control.Monad (replicateM)
+
+--  modules for parsing
+import Data.Char (isDigit)
+import Data.Functor (void)
+import Text.ParserCombinators.ReadP
+  (ReadP
+  ,readP_to_S
+  ,endBy1
+  ,munch1
+  ,satisfy
+  ,many1
+  ,char
+  ,string
+  ,eof
+  ,optional
+  )
+
+type Addr = Int
+data MSet = MSet Addr Int deriving (Show)
+type Mask = String
+data Instr = IM Mask
+             |IS MSet deriving (Show)
+
+printSolution :: Show a => String -> a -> IO ()
+printSolution part sol = putStrLn (part <> ": " <> show sol)
+
+getDatas :: String -> IO [Instr]
+getDatas filename = parseDatas <$> readFile' filename
+
+main :: IO ()
+main = do
+  instrs <- getDatas "day14.txt"
+  printSolution "Part1" (part1 instrs)
+  printSolution "Part2" (part2 instrs)
+
+part1 :: [Instr] -> Int
+part1 instrs = sum memory
+  where
+    -- we fill the memory (an IntMap), in walking along instructions.
+    (_mask, memory) = foldl' f ("", M.empty) instrs
+
+    f (_, mem) (IM mask) = (mask, mem)
+    f (mask, mem) (IS (MSet addr val)) = (mask, M.insert addr val' mem)
+      where
+        val' = (val .&. mAnd) .|. mOr -- apply the mask
+        (mAnd, mOr) = buildMask mask
+
+part2 :: [Instr] -> Int
+part2 instrs = sum memory
+  where
+    (_mask, memory) = foldl' f ("", M.empty) instrs
+
+    f (_, mem) (IM mask) = (mask, mem)
+    f (mask, mem) (IS (MSet addr val)) = (mask, mem')
+      where
+        addresses = genAddr (applyMask mask (intToBinStr addr))
+
+        mem' = foldl' g mem addresses
+
+        g acc x = M.insert x val acc
+
+-- applies the mask for part2. Unlike for part1 we need use string
+-- to manage floatting bits
+applyMask :: String -> String -> String
+applyMask mask addr = zipWith f mask addr
+  where
+    f m a
+      |m == '0' = a  -- 0 unchange the bit
+      |m == '1' = '1' -- 1 set it to one
+      |otherwise = 'X' -- floatting bit
+
+-- We build a list of all possible addresses in taking account
+-- the floatting bits marked with a X.
+genAddr :: String -> [Addr]
+genAddr addr = map f (replicateM n ['0','1'])
+  where
+    n = length (filter (=='X') addr)
+
+    f ls = binStrToInt (fst (foldr g ("",ls) addr))
+
+    g a (m, l@(x:xs))
+      | a /= 'X' = (a:m, l)
+      | otherwise = (x:m, xs)
+    g a (m,[]) = (a:m, [])
+
+
+-- Parsing stuff
+positive :: Read a => ReadP a
+positive = read <$> munch1 isDigit
+
+parseDatas :: String -> [Instr]
+parseDatas str =
+  case readP_to_S parseInstrs str of
+    [x] -> fst x
+    _   -> error "Error: parseDatas: can't parse"
+
+parseInstrs :: ReadP [Instr]
+parseInstrs = do
+  instrs <- many1 parseBlock
+  optional (char '\n')
+  eof
+  pure (concat instrs)
+
+parseBlock :: ReadP [Instr]
+parseBlock = do
+  mask <- IM <$> parseMask
+  void (char '\n')
+  mems <- endBy1 (IS <$> parseMem) (char '\n')
+  pure (mask : mems)
+
+parseMask :: ReadP Mask
+parseMask = string "mask = " *> many1 (satisfy (`elem` "01X"))
+
+parseMem :: ReadP MSet
+parseMem =
+  liftA2 MSet
+         (string "mem[" *> positive)
+         (string "] = " *> positive)
+
+-- other utilities
+-- build the two parts of the mask for part1
+buildMask :: String -> (Int, Int)
+buildMask str = foldl' f (0, 0) str
+  where
+    f (mAnd, mOr) c
+      |c == 'X' = (2*mAnd+1, 2*mOr)
+      |c == '1' = (2*mAnd+1, 2*mOr+1)
+      |c == '0' = (2*mAnd, 2*mOr)
+      |otherwise = error ("Error: buildMask: Invalid character: " <> show c)
+
+intToBinStr :: Int -> String
+intToBinStr 0  = replicate 36 '0'
+intToBinStr n0 = zeros ++ ls
+  where
+    zeros = replicate (36 - length ls) '0'
+    ls = reverse (unfoldr f n0)
+    f 0 = Nothing
+    f n = Just (r', q)
+      where
+        (q, r) = n `divMod` 2
+        r' = if r == 0 then '0' else '1'
+
+binStrToInt :: String -> Int
+binStrToInt str = foldl' f 0 str
+  where
+    f acc c = acc * 2 + c'
+      where
+        c' = if c == '0' then 0 else 1
