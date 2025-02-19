fb1f39d76e5cc321caceb0368f0d2b6e105a359b
enikar <enikar@gresille.org>
2025-02-19 14:53:34 +0100

    Add comments, refactor part1 and part2.

--- hday14.hs
+++ hday14.hs
@@ -1,8 +1,12 @@
 -- AoC 2020, day 14
 
--- this puzzle was not very appealing and astonishingly easy. Thanks to
+-- This puzzle was not very appealing and astonishingly easy. Thanks to
 -- the Applicative instance of List. I would have expected the program to
 -- take longer and the results to be greater, but they fit into an Int64.
+-- However, it's a bit slow. Maybe, we can use ideas from Grabriella
+-- Gonzalez's library foldl to reduce the depth of nested loop:
+-- https://hackage.haskell.org/package/foldl
+-- https://www.haskellforall.com/2013/08/foldl-100-composable-streaming-and.html
 
 {-# LANGUAGE ImportQualifiedPost #-}
 
@@ -17,6 +21,7 @@ import Data.Bits
   ((.&.)
   ,(.|.)
   )
+import Data.IntMap (IntMap)
 import Data.IntMap qualified as M
 import Control.Monad (replicateM)
 
@@ -42,6 +47,10 @@ type Mask = String
 data Instr = IM Mask
              |IS MSet deriving (Show)
 
+-- Although the results fit into a 64 bit integer, we use an Integer.
+type Result = Integer
+type Memory = IntMap Result
+
 printSolution :: Show a => String -> a -> IO ()
 printSolution part sol = putStrLn (part <> ": " <> show sol)
 
@@ -51,44 +60,50 @@ getDatas filename = parseDatas <$> readFile' filename
 main :: IO ()
 main = do
   instrs <- getDatas "day14.txt"
-  printSolution "Part1" (part1 instrs)
-  printSolution "Part2" (part2 instrs)
+  printSolution "Part1" (solve part1 instrs)
+  printSolution "Part2" (solve part2 instrs)
 
-part1 :: [Instr] -> Int
-part1 instrs = sum memory
+-- part1 and part2 are almost the same, so we merge both.
+solve :: (Mask -> Addr -> Int -> Memory -> Memory) -> [Instr] -> Result
+solve updateMem instrs = sum memory
   where
-    -- we fill the memory (an IntMap), in walking along instructions.
-    (_mask, memory) = foldl' f ("", M.empty) instrs
+   (_mask, memory) = foldl' f ("", M.empty) instrs
 
-    f (_, mem) (IM mask) = (mask, mem)
-    f (mask, mem) (IS (MSet addr val)) = (mask, M.insert addr val' mem)
-      where
-        val' = (val .&. mAnd) .|. mOr -- apply the mask
-        (mAnd, mOr) = buildMask mask
+   f (_mask, mem) (IM mask)            = (mask, mem)
+   f (mask, mem)  (IS (MSet addr val)) = (mask, updateMem mask addr val mem)
 
-part2 :: [Instr] -> Int
-part2 instrs = sum memory
+part1 :: Mask -> Addr -> Int -> Memory -> Memory
+part1 mask addr val mem = M.insert addr val' mem
   where
-    (_mask, memory) = foldl' f ("", M.empty) instrs
+    val' = fromIntegral ((val .&. mAnd) .|. mOr) -- apply the mask
+    (mAnd, mOr) = buildMask mask
 
-    f (_, mem) (IM mask) = (mask, mem)
-    f (mask, mem) (IS (MSet addr val)) = (mask, mem')
-      where
-        addresses = genAddr (applyMask mask (intToBinStr addr))
+part2 :: Mask -> Addr -> Int -> Memory -> Memory
+part2 mask addr val mem = foldl' reduce mem addresses
+  where
+    addresses = genAddr (applyMask mask (intToBinStr addr))
+    reduce acc x = M.insert x (fromIntegral val) acc
 
-        mem' = foldl' g mem addresses
+-- utility for part1.
+-- build the two parts of the mask.
+buildMask :: String -> (Int, Int)
+buildMask str = foldl' f (0, 0) str
+  where
+    f (mAnd, mOr) c
+      |c == 'X' = (2*mAnd+1, 2*mOr)
+      |c == '1' = (2*mAnd+1, 2*mOr+1)
+      |otherwise = (2*mAnd, 2*mOr) -- c == '0'
 
-        g acc x = M.insert x val acc
-
--- applies the mask for part2. Unlike for part1 we need use string
--- to manage floatting bits
+-- utilities for part2.
+-- Applies the mask for part2. Unlike for part1 we need to use string to
+-- manage floating bits
 applyMask :: String -> String -> String
 applyMask mask addr = zipWith f mask addr
   where
     f m a
       |m == '0' = a  -- 0 unchange the bit
       |m == '1' = '1' -- 1 set it to one
-      |otherwise = 'X' -- floatting bit
+      |otherwise = 'X' -- set to a floating bit
 
 -- We build a list of all possible addresses in taking account
 -- the floatting bits marked with a X.
@@ -99,11 +114,30 @@ genAddr addr = map f (replicateM n ['0','1'])
 
     f ls = binStrToInt (fst (foldr g ("",ls) addr))
 
+    g a (m,[])    = (a:m, [])
     g a (m, l@(x:xs))
-      | a /= 'X' = (a:m, l)
+      | a /= 'X'  = (a:m, l)
       | otherwise = (x:m, xs)
-    g a (m,[]) = (a:m, [])
 
+-- conversions for part2
+intToBinStr :: Int -> String
+intToBinStr 0  = replicate 36 '0'
+intToBinStr n0 = zeros ++ ls
+  where
+    zeros = replicate (36 - length ls) '0'
+    ls = reverse (unfoldr f n0)
+    f 0 = Nothing
+    f n = Just (r', q)
+      where
+        (q, r) = n `divMod` 2
+        r' = if r == 0 then '0' else '1'
+
+binStrToInt :: String -> Int
+binStrToInt str = foldl' f 0 str
+  where
+    f acc c = acc * 2 + c'
+      where
+        c' = if c == '0' then 0 else 1
 
 -- Parsing stuff
 positive :: Read a => ReadP a
@@ -129,6 +163,8 @@ parseBlock = do
   mems <- endBy1 (IS <$> parseMem) (char '\n')
   pure (mask : mems)
 
+-- hlint insists with some "improvements" I dislike,
+-- so I rewrite these two parsers using Applicative style.
 parseMask :: ReadP Mask
 parseMask = string "mask = " *> many1 (satisfy (`elem` "01X"))
 
@@ -137,33 +173,3 @@ parseMem =
   liftA2 MSet
          (string "mem[" *> positive)
          (string "] = " *> positive)
-
--- other utilities
--- build the two parts of the mask for part1
-buildMask :: String -> (Int, Int)
-buildMask str = foldl' f (0, 0) str
-  where
-    f (mAnd, mOr) c
-      |c == 'X' = (2*mAnd+1, 2*mOr)
-      |c == '1' = (2*mAnd+1, 2*mOr+1)
-      |c == '0' = (2*mAnd, 2*mOr)
-      |otherwise = error ("Error: buildMask: Invalid character: " <> show c)
-
-intToBinStr :: Int -> String
-intToBinStr 0  = replicate 36 '0'
-intToBinStr n0 = zeros ++ ls
-  where
-    zeros = replicate (36 - length ls) '0'
-    ls = reverse (unfoldr f n0)
-    f 0 = Nothing
-    f n = Just (r', q)
-      where
-        (q, r) = n `divMod` 2
-        r' = if r == 0 then '0' else '1'
-
-binStrToInt :: String -> Int
-binStrToInt str = foldl' f 0 str
-  where
-    f acc c = acc * 2 + c'
-      where
-        c' = if c == '0' then 0 else 1
